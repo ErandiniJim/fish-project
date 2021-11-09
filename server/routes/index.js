d1e514854cff2b08f3542801a86f792c7910b275
ErandiniJim <erandinijim99@gmail.com>
2021-11-09 00:49:36 -0600

    instalacion webpack

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author: 'Erandini', appName: 'Fishapp' });
+  res.render('index', { title: 'Express', author: 'Erandini', appName: 'Fishapp', company: 'Maritima' });
 });
 
 /* Agregando nueva ruta;*/
